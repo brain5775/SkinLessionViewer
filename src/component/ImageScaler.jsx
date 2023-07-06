1a1d76525b4f1bb81a254446025b28597fdea13b
brain5775 <79450158+brain5775@users.noreply.github.com>
2023-07-06 22:02:00 +0700

    update: - scale image if has attribute of x, y, height and width

--- src/component/ImageScaler.jsx
+++ src/component/ImageScaler.jsx
@@ -11,44 +11,49 @@ const ImageScaler = (props) => {
     const svgElement = htmlElement.querySelector("svg");
     const svgWidth = svgElement.getAttribute("width");
     const svgHeight = svgElement.getAttribute("height");
+    let scaler;
     if (svgElement) {
       svgElement.setAttribute("width", "300");
       svgElement.setAttribute("height", "300");
+      const childSVGElement = svgElement.children;
+      if (svgWidth > 300 || svgHeight > 300) {
+        const scale1 = 300 / svgWidth;
+        const scale2 = 300 / svgHeight;
+        const totalScale = Math.min(scale1, scale2);
+        scaler = totalScale;
+      }
+
+      for (let i = 0; i < childSVGElement.length; i++) {
+        if (i !== 0) {
+          for (
+            let att,
+              j = 0,
+              atts = childSVGElement[i].attributes,
+              n = atts.length;
+            j < n;
+            j++
+          ) {
+            att = atts[j];
+            // console.log(att.nodeValue);
+            if (
+              att.nodeName === "x" ||
+              att.nodeName === "y" ||
+              att.nodeName === "width" ||
+              att.nodeName === "height"
+            ) {
+              att.nodeValue *= scaler;
+            }
+          }
+        }
+      }
     }
 
     const imageElement = svgElement.querySelector("image");
     if (imageElement) {
-      if (svgWidth > 300 || svgHeight > 300) {
-        const scale1 = 300 / svgWidth;
-        const scale2 = 300 / svgHeight;
-        const totalScale = Math.min(scale1, scale2);
-
-        const newImageWidth = svgWidth * totalScale;
-        const newImageHeight = svgHeight * totalScale;
-        imageElement.setAttribute("width", newImageWidth);
-        imageElement.setAttribute("height", newImageHeight);
-      }
-    }
-    const rectElement = svgElement.querySelector("rect");
-    const rectWidth = rectElement.getAttribute("width");
-    const rectHeight = rectElement.getAttribute("height");
-    const rectX = rectElement.getAttribute("x");
-    const rectY = rectElement.getAttribute("y");
-    if (rectElement) {
-      if (svgWidth > 300 || svgHeight > 300) {
-        const scale1 = 300 / svgWidth;
-        const scale2 = 300 / svgHeight;
-        const totalScale = Math.min(scale1, scale2);
-
-        const newRectWidth = rectWidth * totalScale;
-        const newRectHeight = rectHeight * totalScale;
-        const newRectX = rectX * totalScale;
-        const newRectY = rectY * totalScale;
-        rectElement.setAttribute("width", newRectWidth);
-        rectElement.setAttribute("height", newRectHeight);
-        rectElement.setAttribute("x", newRectX);
-        rectElement.setAttribute("y", newRectY);
-      }
+      const newImageWidth = svgWidth * scaler;
+      const newImageHeight = svgHeight * scaler;
+      imageElement.setAttribute("width", newImageWidth);
+      imageElement.setAttribute("height", newImageHeight);
     }
 
     svgRef.current.appendChild(htmlElement);
